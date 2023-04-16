add0cd3f0f5dbc81fc8fe03536c5d577e616c3ef
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-04-16 21:52:40 +0300

    Task 1.3

--- HomeWork001.java
+++ HomeWork001.java
@@ -1,9 +1,12 @@
 package HomeWorkGB;
 
+import java.util.ArrayList;
+
 public class HomeWork001 {
     public static void main(String[] args) {
         System.out.println(TriangNumber(8)); // Task 1.1
         System.out.println(Fact(5)); // Task 1.2
+        System.out.println(SimpleNum(1000)); // Task 1.3
     }
 
     public static int TriangNumber(int a) {
@@ -13,5 +16,24 @@ public class HomeWork001 {
     public static int Fact(int a) {
         if (a>1) return Fact(a-1)*a;
         return 1;
+    }
+
+    public static ArrayList SimpleNum(int a) {
+        ArrayList<Integer> list_result = new ArrayList<Integer>();
+        int count = 1;
+        list_result.add(count);
+        while (count<a) {
+            count++;
+            int temp = 2;
+            int count_temp = 0;
+            while(temp<=count){
+                if (count%temp == 0){
+                    count_temp++;
+                }
+                temp++;
+            }
+            if (count_temp == 1) list_result.add(count);
+        }
+        return list_result;
     }
 }
