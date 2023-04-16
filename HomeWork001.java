621ade7a7cecb95714a812210d4f1313fc28b034
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-04-16 21:31:40 +0300

    Task 1.1 and 1.2

--- HomeWork001.java
+++ HomeWork001.java
@@ -1,5 +1,17 @@
 package HomeWorkGB;
 
 public class HomeWork001 {
-    
+    public static void main(String[] args) {
+        System.out.println(TriangNumber(8)); // Task 1.1
+        System.out.println(Fact(5)); // Task 1.2
+    }
+
+    public static int TriangNumber(int a) {
+        return a*(a+1)/2;
+    }
+
+    public static int Fact(int a) {
+        if (a>1) return Fact(a-1)*a;
+        return 1;
+    }
 }
