05694ff4063750333557d67e422eb400a6b2dafa
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-04-19 22:54:48 +0300

    Edit

--- HomeWork002.java
+++ HomeWork002.java
@@ -1,13 +1,24 @@
 package HomeWorkGB;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class HomeWork002 {
     public static void main(String[] args) {
-        String input = "[{ \"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"},{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"},{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
-        ParseString(input);
+        String path = "input.txt";
+        OutConsole(ParseString(ReaderString(path)));
+        OutFile(ParseString(ReaderString(path)));
     }
-    public static void ParseString(String str) {
+    public static ArrayList<ArrayList> ParseString(String str) {
         str = str.replaceAll("\"", "").replaceAll("}", "");
         char [] charArr = str.toCharArray();
         StringBuilder sb = new StringBuilder();
@@ -34,6 +45,51 @@ public class HomeWork002 {
                 flag = true;
             }
         }
-        System.out.println(arrList);
+        return arrList;
+    }
+    public static String ReaderString(String path) {
+        File file = new File(path);
+        try (BufferedReader br =new BufferedReader(new FileReader(file,StandardCharsets.UTF_8))) {
+            return br.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log("Error reader");
+            return "";
+        }
+    }
+    public static void OutConsole(ArrayList<ArrayList> ArrStr) {
+        for (ArrayList <String> arrayList : ArrStr) {
+            System.out.println("Студент "+ arrayList.get(0) +
+              " получил "+ arrayList.get(1) +  " по предмету "+ arrayList.get(2) +  ".");
+        }
+    }
+    public static void OutFile(ArrayList<ArrayList> ArrStr) {
+
+        String path = "output.txt";
+        try (FileWriter fileWriter = new FileWriter(path, StandardCharsets.UTF_8)) {
+            for (ArrayList <String> arrayList : ArrStr) {
+                fileWriter.append("Студент "+ arrayList.get(0) +
+                                " получил "+ arrayList.get(1) +  
+                                " по предмету "+ arrayList.get(2) +  ". \n");
+            }
+            fileWriter.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log("Error writer");
+        }
+    }
+    public static void Log(String mess) { // От вопросов в лог-файле так и не получилось избавиться х(
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("log.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SimpleFormatter formatter = new SimpleFormatter();
+        fileHandler.setFormatter(formatter);
+        Logger logger = Logger.getAnonymousLogger();
+        logger.addHandler(fileHandler);
+        logger.log(Level.INFO, mess);
+        fileHandler.close();
     }
 }
