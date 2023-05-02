9675b12013d935008fe1c5374dbc65b41024ec68
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-05-02 20:45:43 +0300

    add HomeWork005

---
+++ PhoneBook.java
@@ -0,0 +1,13 @@
+package HomeWorkGB;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneBook {
+    private Map<String, ArrayList<Integer>> map = new HashMap<>();
+
+    void add (String name){
+        
+    }
+}
