421c76ca2e3442c4d6c0e0ee999f9047f59d28d3
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-05-22 19:28:46 +0300

    Add NoteBook

---
+++ Notebook.java
@@ -0,0 +1,31 @@
+package HomeWorkGB;
+
+public class Notebook {
+    private byte ram;
+    private int hdd;
+    private String system;
+    private String videocard;
+    private String proc;
+    private float rate;
+
+    public Notebook(byte ram, int hdd, String system,
+                     String videocard, String proc, float rate){
+        this.ram = ram;
+        this.hdd = hdd;
+        this.system = system;
+        this.videocard = videocard;
+        this.proc = proc;
+        this.rate = rate;
+    }
+
+    public String getSystem() {
+        return system;
+    }
+
+    @Override
+    public String toString() {
+        return "processor: " + proc + ",\nrate: " + rate + 
+                ",\nRAM: " + ram +  ",\nsystem: " + system + 
+                ",\nVideo: " + videocard + ",\nHDD: " + hdd;
+    }
+}
